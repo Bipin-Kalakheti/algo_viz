e733d805c9935fdd12ce68cca4b1082b8c75bfb5
Bipin Kalakheti <100770518+Bipin-Kalakheti@users.noreply.github.com>
2025-01-02 18:46:08 -0500

    add PathfindingProvider to App component and create usePathfinding hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { PathfindingProvider } from "./context/PathfindingContext";
+
 function App() {
   return (
-    <>
+    <PathfindingProvider>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    </PathfindingProvider>
   );
 }
 
